c4931fa463175d7082d51740e7d80e03307cba31
Santiagozm16 <55797102+Santiagozm16@users.noreply.github.com>
2024-03-18 14:06:52 -0500

    Update 2
    Subi Avatar, Coloque Icono y cambie descripciones y enlaces.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let emptyElement = document.querySelector('.about .empty');
 let titleElement = document.querySelector('.about .title');
 let figureElement = document.querySelectorAll('.services figure'); //Cargamos todos los elementos de etiqueta figure que estan contenidos en la clase servicios
+let imageElement = document.querySelector('figure .avatar');
+let imageElement2 = document.querySelector('.about .avatar img');
 
+console.log(imageElement2);
 window.addEventListener('mousemove', react);
 
 function react(value){ //value toma todos los valores que mousemove tenga
@@ -11,6 +14,7 @@ function react(value){ //value toma todos los valores que mousemove tenga
         element.style.flexBasis = (window.innerWidth - value.clientX) +'px';
     });
 
+    imageElement.style.padding = value.clientX / 2 - 'px';
     emptyElement.style.flexBasis = value.clientX + 'px'; //.clientX es la posicion en X en que esta mouse, es una de las propiedades del objeto mousemove
     titleElement.style.flexBasis = value.clientY / 2 + 'px';
 }
