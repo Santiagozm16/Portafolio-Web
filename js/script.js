dea00b03667f1e386a0f3decd2d5f1570c888390
Santiagozm16 <55797102+Santiagozm16@users.noreply.github.com>
2024-08-06 22:51:21 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,21 +10,32 @@ function react(value){ //value toma todos los valores que mousemove tenga
     //console.log(value.clientX);
 
     figureElement.forEach(function(element){ //figureElement es un array donde estan los elementos de etiquera figure // Element es el elmento individual es este array ya que se recorre con un forEach
-        element.style.flexBasis = (window.innerWidth - value.clientX) +'px';
+        if(value.clientX > 800 && value.clientX < 1100){
+            element.style.flexBasis = (window.innerWidth - value.clientX) +'px';
+        }
     });
 
     imageElement.style.padding = value.clientX / 2 - 'px';
-    emptyElement.style.flexBasis = value.clientX + 'px'; //.clientX es la posicion en X en que esta mouse, es una de las propiedades del objeto mousemove
+    if(value.clientX > 800 && value.clientX < 1100){
+        emptyElement.style.flexBasis = value.clientX + 'px'; //.clientX es la posicion en X en que esta mouse, es una de las propiedades del objeto mousemove
+    }
     titleElement.style.flexBasis = value.clientY / 2 + 'px';
 }
 
+
+var menuAbierto = false;
 var servicios = document.getElementById("Servicios");
 servicios.addEventListener('click', function(){
     let menu = document.querySelectorAll(".menu");
-    menu.forEach(function(elemento){
-        elemento.style.animation = 'desplegarMenu 0.5s forwards';
-        elemento.style.display = 'block';
-    })
+    if (menuAbierto == true){
+        cerrarMenu();
+    }else {
+        menuAbierto = true; 
+        menu.forEach(function(elemento){
+            elemento.style.animation = 'desplegarMenu 0.5s forwards';
+            elemento.style.display = 'block';
+        })
+    }
 })
 
 function cerrarMenu(){
@@ -32,13 +43,40 @@ function cerrarMenu(){
     menu.forEach(function(elemento){
         elemento.style.animation = 'cerrarMenu 0.5s forwards';
     })
+    menuAbierto = false;
 }
 
-var a = document.querySelectorAll('[id="lista"]');
-a.forEach(function(elemento){
-    elemento.addEventListener('click', cerrarMenu);
-    //console.log(elemento);
-})
+
+var enlaces = document.querySelectorAll('#div_menu a');
+enlaces.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        // Remove 'sticky' y 'Nosticky' classes de todas las secciones
+        enlaces.forEach(function(e){
+            let targetId2 = e.getAttribute('href').substring(1);
+            document.getElementById(targetId2).classList.remove('sticky');
+            document.getElementById(targetId2).classList.remove('Nosticky');
+        });
+
+        // Scroll a la sección objetivo con una animación suave
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+
+        // Agregar 'sticky' a la sección objetivo después de que la animación haya terminado
+        setTimeout(function() {
+            // Agregar 'sticky' a todas las secciones
+            enlaces.forEach(function(e){
+                let targetId2 = e.getAttribute('href').substring(1);
+                document.getElementById(targetId2).classList.add('sticky');
+                document.getElementById(targetId2).classList.remove('Nosticky');
+            });
+        }, 100);
+
+        cerrarMenu();
+    });
+});
 
 // Detectar tecla F5 y Ctrl+R
 window.addEventListener('keydown', function(event) {
@@ -56,4 +94,12 @@ window.addEventListener('load', function() {
             return;
             //window.location.href = 'index.html';
         }
+});
+
+document.querySelector('body').addEventListener('click', function(elemento){
+    if(elemento.target.closest('.menu') || elemento.target.id == 'Servicios'){ //Si se hace click en la zona que contiene la clase menu o en el enlace de Servicios el menu no se cierra
+        return;
+    }else{ // Si hacemos click fuera del menu de servicios el menu se cierra.
+        cerrarMenu();
+    }
 });
